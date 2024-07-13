58e9e0f113b40a7ca5552f063c227776d1332b1d
mo <moe.rifaii77@gmail.com>
2024-07-14 00:19:35 +0300

    merge FileReader and BufferedFileReader comparison tests and add Scanner demo

--- src/test/java/dev/rifaii/io/IOBasicsTest.java
+++ src/test/java/dev/rifaii/io/IOBasicsTest.java
@@ -5,15 +5,18 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Scanner;
 
 /*
  * There are many ways to read a file in Java:
  * InputStream (FileInputStream)
  * FileReader
+ * BufferedReader/BufferedInputStream
  * Scanner
- * ....
  */
 public class IOBasicsTest {
 
@@ -26,9 +29,15 @@ public class IOBasicsTest {
 
     @BeforeAll
     static void setup() {
+        if (!"UTF-8".equals(Charset.defaultCharset().displayName())) {
+            System.setProperty("file.encoding", "UTF-8");
+        }
         Assertions.assertEquals("UTF-8", System.getProperty("file.encoding"));
     }
 
+    /**
+     * @see InputStream reads only one BYTE at a time
+     */
     @Test
     void fileInputStream() throws IOException {
         File file = new File(RAND_FILE_NAME);
@@ -41,6 +50,9 @@ public class IOBasicsTest {
         }
     }
 
+    /**
+     * @see FileReader reads one CHARACTER at a time
+     */
     @Test
     void fileReader() throws IOException {
         File file = new File(RAND_FILE_NAME);
@@ -53,40 +65,56 @@ public class IOBasicsTest {
         }
     }
 
-    @Test //FAILS
+    /**
+     * How InputStream can fail:
+     */
+    @Test //SHOULD FAIL
     void fileReaderVsFileInputStream1() throws IOException {
         File file = new File(CAR_FILE_NAME);
         Assertions.assertTrue(file.exists());
 
         try (InputStream inputStream = new FileInputStream(file)) {
             int byt = inputStream.read();
-            Assertions.assertAll(() -> {
-                Assertions.assertEquals(O_TILDE_DECIMAL_REP, byt);
-                Assertions.assertEquals("Õ", Character.toString(byt));
-            });
+            Assertions.assertEquals(O_TILDE_DECIMAL_REP, byt);
+            Assertions.assertEquals("Õ", Character.toString(byt));
         }
     }
 
+    /**
+     * @see FileReader reads one CHARACTER at a time
+     * Regardless of how many bytes the character is.
+     * Unlike InputStream which only reads one byte at a time.
+     *
+     * @see FileReader uses the default system charset for character
+     * encoding unless another charset is specified through the constructor:
+     * @see FileReader#FileReader(File, Charset)
+     *
+     * So in summary, it's InputStream wrapper with character encoding
+     */
     @Test
     void fileReaderVsFileInputStream2() throws IOException {
         File file = new File(CAR_FILE_NAME);
         Assertions.assertTrue(file.exists());
 
-        try (FileReader fileReader = new FileReader(file)) {
+        try (FileReader fileReader = new FileReader(file, StandardCharsets.UTF_8)) {
             int byt = fileReader.read();
-            Assertions.assertAll(() -> {
-                Assertions.assertEquals(O_TILDE_DECIMAL_REP, byt);
-                Assertions.assertEquals("Õ", Character.toString(byt));
-            });
+            Assertions.assertEquals(O_TILDE_DECIMAL_REP, byt);
+            Assertions.assertEquals("Õ", Character.toString(byt));
         }
     }
 
+    /*
+     * What about BufferedReader?
+     * BufferedReader Reads chunks of data into a buffer which
+     * makes it faster than FileReader and FileInputStream
+     */
     @Test
     void fileReaderForLargeFiles() throws IOException {
         File file = new File(LARGE_FILE_NAME);
         Assertions.assertTrue(file.exists());
 
-        var start = Instant.now();
+        //Reading large file using FileReader
+        var startFileReader = Instant.now();
         try (FileReader fileReader = new FileReader(file)) {
             int byt = fileReader.read();
             while (byt != -1) {
@@ -94,21 +122,10 @@ public class IOBasicsTest {
                 byt = fileReader.read();
             }
         }
-        var end = Instant.now();
+        var endFileReader = Instant.now();
 
-        Assertions.assertTrue(Duration.between(start, end).toMillis() < 1000);
-    }
-
-    /*
-     * Reads chunks of data into a buffer that is stored in memory
-     * which makes it faster than FileReader and FileInputStream
-     */
-    @Test
-    void bufferedReaderForLargeFiles() throws IOException {
-        File file = new File(LARGE_FILE_NAME);
-        Assertions.assertTrue(file.exists());
-
-        var start = Instant.now();
+        //Reading large file using BufferedReader
+        var startBufferedReader = Instant.now();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             int byt = bufferedReader.read();
             while (byt != -1) {
@@ -116,18 +133,47 @@ public class IOBasicsTest {
                 byt = bufferedReader.read();
             }
         }
+        var endBufferedReader = Instant.now();
 
-        var end = Instant.now();
+        Duration bufferedReaderDuration = Duration.between(startBufferedReader, endBufferedReader);
+        Duration fileReaderDuration = Duration.between(startFileReader, endFileReader);
+        Assertions.assertTrue(bufferedReaderDuration.compareTo(fileReaderDuration) < 0);
 
-        Assertions.assertTrue(Duration.between(start, end).toMillis() < 1000);
+        System.out.printf("%nBufferedReader duration: %s", bufferedReaderDuration.toMillis());
+        System.out.printf("%nFileReader duration: %s%n", fileReaderDuration.toMillis());
+    }
+
+    /**
+     * The Scanner class provides a convenient API
+     * that uses regex under the hood to read meaningful data
+     * and avoid manual parsing. Like everything else it uses
+     * @see InputStream and uses system default charset unless
+     * another charset is specified.
+     *
+     * @see Scanner#nextInt()
+     * @see Scanner#nextBoolean()
+     * @see Scanner#nextBigDecimal()
+     * @see Scanner#nextLine()
+     */
+    @Test
+    void scanner() throws IOException {
+        File file = new File(CAR_FILE_NAME);
+        Assertions.assertTrue(file.exists());
+
+        try (Scanner scanner = new Scanner(file, StandardCharsets.UTF_8)) {
+            while (scanner.hasNext()) {
+                System.out.print(scanner.next());
+            }
+            System.out.println("\n");
+        }
     }
 
     /*
      * Summary:
      * InputStream reads 1 byte at a time
-     * Reader reads 1 character at a time (uses InputStream)
-     * BufferedReader loads a chunk of chars into memory and streams then one at a time (uses InputStream)
-     * Scanner uses Regex to read specific data types (int, double etc...) instead of just plain bytes or chars
+     * Reader reads 1 character at a time and allows to specify charset (uses InputStream)
+     * BufferedReader loads a chunk of chars into memory and streams them one at a time (uses InputStream)
+     * Scanner uses Regex to read specific data types (int, double etc...) instead of just plain bytes or chars (uses InputStream)
      */
 
 }
